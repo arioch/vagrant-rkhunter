c4bcea5bbac6e4585fa3019686112eb70e290ddf
Karen Harutyunyan <karen@inuits.eu>
2015-02-23 14:24:35 +0100

    adding syslog

--- modules/profiles/manifests/rkhunter.pp
+++ modules/profiles/manifests/rkhunter.pp
@@ -2,9 +2,11 @@
 class profiles::rkhunter {
 
   $root_email = hiera('root_email')
+  $syslog = hiera('syslog')
 
   class { '::rkhunter':
-    rootEmail => "${root_email}",
+    rootEmail    => "${root_email}",
+    remoteSyslog => "${syslog}",
   }
 
 }
