f5e92c253dd43f4b7569c9932e291ffe67a0027c
Tom De Vylder <tom@penumbra.be>
2015-03-02 11:41:28 +0100

    Strip out hiera function calls

--- puppet/modules/profiles/manifests/rkhunter.pp
+++ puppet/modules/profiles/manifests/rkhunter.pp
@@ -1,39 +1,31 @@
 #
 class profiles::rkhunter {
-
   include ::rkhunter
 
- if hiera('rkhunter::cron_propupd') {
-
-  cron { 'rkhunter propupd':
-    command => 'rkhunter --propupd | logger -t RKHUNTER_PROPUPD',
-    user    => root,
-    hour    => '2',
-    minute  => '10',
+  if ::rkhunter::cron_propupd {
+    cron { 'rkhunter propupd':
+      command => 'rkhunter --propupd | logger -t RKHUNTER_PROPUPD',
+      user    => root,
+      hour    => '2',
+      minute  => '10',
+    }
   }
 
+  if ::rkhunter::cron_rpm {
+    cron { 'rkhunter pkgmgr RPM':
+      command => 'rkhunter --propupd --pkgmgr RPM | logger -t RKHUNTER_RPM',
+      user    => root,
+      hour    => '2',
+      minute  => '15',
+    }
   }
 
-  if hiera('rkhunter::cron_rpm') {
-  
-  cron { 'rkhunter pkgmgr RPM':
-    command => 'rkhunter --propupd --pkgmgr RPM | logger -t RKHUNTER_RPM',
-    user    => root,
-    hour    => '2',
-    minute  => '15',
+  if ::rkhunter::cron_check {
+    cron { 'rkhunter check':
+      command => 'rkhunter --check --sk | logger -t RKHUNTER_CHECK',
+      user    => root,
+      hour    => '2',
+      minute  => '20',
+    }
   }
-
-  }
-
-  if hiera('rkhunter::cron_check') {
-  
-  cron { 'rkhunter check':
-    command => 'rkhunter --check --sk | logger -t RKHUNTER_CHECK',
-    user    => root,
-    hour    => '2',
-    minute  => '20',
-  }
-
-  }
-
 }
