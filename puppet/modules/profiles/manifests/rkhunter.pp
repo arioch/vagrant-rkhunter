e1c729cf761a0e615bf362b4939d6362c2606945
Karen Harutyunyan <karen@inuits.eu>
2015-03-02 09:30:39 +0100

    change hiera variabl.

--- puppet/modules/profiles/manifests/rkhunter.pp
+++ puppet/modules/profiles/manifests/rkhunter.pp
@@ -3,11 +3,7 @@ class profiles::rkhunter {
 
   include ::rkhunter
 
-$rkhunter_cron_propupd = hiera('rkhunter::cron_propupd')
-$rkhunter_cron_rpm = hiera('rkhunter::cron_rpm')
-$rkhunter_cron_check = hiera('rkhunter::cron_check')
-
- if $rkhunter_cron_propupd {
+ if hiera('rkhunter::cron_propupd') {
 
   cron { 'rkhunter propupd':
     command => 'rkhunter --propupd | logger -t RKHUNTER_PROPUPD',
@@ -18,7 +14,7 @@ $rkhunter_cron_check = hiera('rkhunter::cron_check')
 
   }
 
-  if $rkhunter_cron_rpm {
+  if hiera('rkhunter::cron_rpm') {
   
   cron { 'rkhunter pkgmgr RPM':
     command => 'rkhunter --propupd --pkgmgr RPM | logger -t RKHUNTER_RPM',
@@ -29,7 +25,7 @@ $rkhunter_cron_check = hiera('rkhunter::cron_check')
 
   }
 
-  if $rkhunter_cron_check {
+  if hiera('rkhunter::cron_check') {
   
   cron { 'rkhunter check':
     command => 'rkhunter --check --sk | logger -t RKHUNTER_CHECK',
